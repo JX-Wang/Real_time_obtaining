874cb7af383400c2d52e271d8abd7b1e8be24dd2
WangJunXiong <1411349759@qq.com>
2019-09-26 17:58:18 +0800

    ver _0.1

---
+++ real_time_obtaining/real_time_obtaining.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# coding:utf-8
+"""
+whois client API server
+=======================
+Author@Wangjunxiong
+Data@2019.9.26
+"""
+
+from flask import Flask
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+
+monkey.patch_all()
+app = Flask("Obtaining Dns")
+app.config.update(DEBUG=True)
+
+file_name = "effective_ApiNode.txt"
+_port = 8048
+
+
+def start():
+    """API start"""
+    _start = "API Server start\n"
+    _start += "* For dns obtaining\n"
+    _start += "* wud@WangJunxiong\n"
+    http_server = WSGIServer(('', _port), app)
+    http_server.serve_forever()
+
+
+@app.route('/monitor/realtime/<domain>')
+def obtaining(domain):
+    """Get domain dns"""
+
+    pass
+
+
+@app.route('/check')
+def check():
+    """Get server status.
+       but we this instance we do not need now.
+    """
+
+
+if __name__ == '__main__':
+    start()
